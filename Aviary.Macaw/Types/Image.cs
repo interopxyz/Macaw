dbd6a8d294dbdb637cf5018ac9824c08b90bfd96
Mans-TTCore <36418123+Mans-TTCore@users.noreply.github.com>
2019-10-01 23:46:34 -0400

    WIP Image conversion

--- Aviary.Macaw/Types/Image.cs
+++ Aviary.Macaw/Types/Image.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Macaw
+namespace Aviary.Macaw
 {
     public class Image
     {
+
+        #region members
+
+        protected Bitmap bitmap = new Bitmap(100, 100);
+
+        #endregion
+
+        #region constructors
+
+        public Image()
+        {
+
+        }
+
+        public Image(Image image)
+        {
+            this.Bitmap = image.bitmap;
+        }
+
+        public Image(Bitmap bitmap)
+        {
+            this.Bitmap = bitmap;
+        }
+
+        #endregion
+
+        #region properties
+
+        public virtual Bitmap Bitmap
+        {
+            get { return (Bitmap)bitmap.Clone(); }
+            set { bitmap = (Bitmap)value.Clone(); }
+        }
+
+        #endregion
+
+        #region members
+
+
+
+        #endregion
+
+        #region overrides
+
+        public override string ToString()
+        {
+            return "Image ("+bitmap.Width+"x"+bitmap.Height+")";
+        }
+
+        #endregion
+
     }
 }
